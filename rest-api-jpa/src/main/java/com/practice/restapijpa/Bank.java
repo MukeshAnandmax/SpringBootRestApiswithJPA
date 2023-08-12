0caf8e4bf2bc7e0cc2cd5e7d14bc4c76ae3510d2
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-08-12 13:48:24 +0530

    Generation type Auto -> Identity

--- rest-api-jpa/src/main/java/com/practice/restapijpa/Bank.java
+++ rest-api-jpa/src/main/java/com/practice/restapijpa/Bank.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Bank {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bankId;
     private  String bankName;
 
