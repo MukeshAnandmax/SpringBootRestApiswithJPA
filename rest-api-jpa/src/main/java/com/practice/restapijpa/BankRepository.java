4d1a992895a719ad9a687a85f20fda5139cb7078
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-08-12 21:29:43 +0530

    get All bank method added

--- rest-api-jpa/src/main/java/com/practice/restapijpa/BankRepository.java
+++ rest-api-jpa/src/main/java/com/practice/restapijpa/BankRepository.java
@@ -3,10 +3,16 @@ package com.practice.restapijpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface BankRepository extends JpaRepository<Bank,Integer> {
 
     @Query(value = "select * from bank b where b.bank_id=:bankId",nativeQuery = true)
     public  Bank findByBankId(int bankId);
+
+
+    @Query(value = "select b from Bank b")
+    public List<Bank> getAllBank();
 
 
 }
