3a745502fa73ef774ad91992d94a01f40a16c7fc
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-08-12 21:27:14 +0530

    to String added

--- rest-api-jpa/src/main/java/com/practice/restapijpa/Bank.java
+++ rest-api-jpa/src/main/java/com/practice/restapijpa/Bank.java
@@ -1,11 +1,14 @@
 package com.practice.restapijpa;
 
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@ToString
 public class Bank {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
